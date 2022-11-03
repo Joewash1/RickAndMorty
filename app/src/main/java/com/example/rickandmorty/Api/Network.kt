64971a8a807bbe5aefb78800a6bf7cffab6fa5aa
Joewash1 <joeywashing@yahoo.com>
2022-11-03 16:39:10 -0500

    added networking and UI changes to display

--- app/src/main/java/com/example/rickandmorty/Api/Network.kt
+++ app/src/main/java/com/example/rickandmorty/Api/Network.kt
@@ -2,12 +2,14 @@ package com.example.rickandmorty.Api
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object Network {
     val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-    val retrofit = Retrofit.Builder()
+    val retrofit = Retrofit.Builder().client(getHttpClient())
         .baseUrl("https://rickandmortyapi.com/api/")
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
@@ -15,4 +17,12 @@ object Network {
     val service: Service by lazy { retrofit.create(Service::class.java) }
 
     val rickAndMortyClient = RickAndMortyClient(service)
+
+    private fun getHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        })
+        return builder.build()
+    }
 }
